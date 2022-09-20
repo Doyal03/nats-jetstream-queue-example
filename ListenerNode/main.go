49bd6e21e0d29be7d0e429ce0133d42049b6ef0b
Debashree W <doyalw@gmail.com>
2022-09-21 00:00:30 +0530

    initial

---
+++ ListenerNode/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nats-io/nats.go"
+	"runtime"
+)
+
+func main() {
+	conn, err := nats.Connect(nats.DefaultURL) // localhost:4222
+	if err != nil {
+		panic(err)
+	}
+	count := 1
+
+	conn.QueueSubscribe("foo", "hellos", func(msg *nats.Msg) {
+		fmt.Printf("[%v] %v\n", count, string(msg.Data))
+		count++
+		conn.Publish(msg.Reply, []byte("Message delivered"))
+	})
+
+	runtime.Goexit()
+	defer conn.Close()
+}
